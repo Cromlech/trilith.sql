4d876e180908c4da417fe14c8207e44ae9193d08
Souheil CHELFOUH <trollfot@gmail.com>
2016-04-18 20:40:53 +0200

    Safely assuming on the base implementation of __contains__ that the primary_key name is 'id'. Subclasses are free to override in case of multiple keys or other names

--- src/trilith/sql/oauth2/stores.py
+++ src/trilith/sql/oauth2/stores.py
@@ -5,6 +5,8 @@ from cromlech.sqlalchemy import SQLAlchemySession
 from trilith.sql.oauth2 import models
 from trilith.oauth2.interfaces import IUsers, IGrants, ITokens, IClients
 from zope.interface import implementer
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.exc import MultipleResultsFound
 
 
 class Storage(object):
@@ -15,12 +17,19 @@ class Storage(object):
         self.engine = engine
 
     def find(self, **params):
-        with SQLAlchemySession(self.engine) as s:
-            q = session.query(self.__factory__)
-            for key, value in params.items():
-                q = q.filter(getattr(myClass, attr) == value)
-            found = q.one()
-        return found
+        try:
+            with SQLAlchemySession(self.engine) as s:
+                q = session.query(self.__factory__)
+                for key, value in params.items():
+                    q = q.filter(getattr(myClass, attr) == value)
+                    found = q.one()
+        except NoResultFound:
+            found = None
+        except MultipleResultsFound:
+            # Might need logging
+            found = None
+        finally:
+            return found
 
     def add(self, **data):
         item = self.__factory__(**data)
@@ -44,7 +53,11 @@ class Storage(object):
                 raise StopIteration()
 
     def __contains__(self, key):
-        raise NotImplementedError('Implement your own')
+        # Assumes that the primary_key is 'id'
+        # In case it's not true, override in subclass.
+        with SQLAlchemySession(self.engine) as s:
+            q = s.query(self.__factory__.id).filter(self.__factory__.id == key)
+            return bool(s.query(q.exists()))
 
     def get(self, key, default=None):
         try:
@@ -55,40 +68,21 @@ class Storage(object):
 
 @implementer(IUsers)
 class Users(Storage):
-
     __factory__ = models.User
-
-    def __contains__(self, key):
-        pass
 
 
 @implementer(IClients)
 class Clients(Storage):
-
     __factory__ = models.Client
-
-    def __contains__(self, key):
-        with SQLAlchemySession(self.engine) as s:
-            q = s.query(self.__factory__.id).filter(self.__factory__.id == key)
-            return bool(s.query(q.exists()))
-
-
-@implementer(ITokens)
-class Tokens(Storage):
-
-    __factory__ = models.Token
-
-    add = NotImplementedError("You can't manually add a token")
-
-    def __contains__(self, key):
-        pass
 
 
 @implementer(IGrants)
 class Grants(Storage):
-
     __factory__ = models.Grant
 
-    def __contains__(self, key):
-        pass
 
+@implementer(ITokens)
+class Tokens(Storage):
+    __factory__ = models.Token
+
+    add = NotImplementedError("You can't manually add a token")
